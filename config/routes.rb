d4661b8cd402bc9e2eb4fced0326342c16ed341c
j.h.nogami <j.h.nogami@gmail.com>
2019-09-23 17:09:57 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  get '/signup' => "users#new"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  resources :users
+
+
+  get '/signup' => "users#new"
+  post "/signup" => "user#create"
 end
